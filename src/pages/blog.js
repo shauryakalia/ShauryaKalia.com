e954c767a9e5c1c8776e132f0c9235d89e153011
shauryakalia <shauryakalia@gmail.com>
2021-04-04 16:29:04 +0530

    blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,10 +2,39 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = ({
+  data: {
+    allStrapiBlogs: { nodes: blogs }
+  }
+}) => {
+  return <Layout>
+    <section className="blog-page">
+      <Blogs  blogs={blogs} title="blogs" />
+    </section>
+  </Layout> 
 }
+
+export const query = graphql`
+  {
+    allStrapiBlogs(sort: {fields: date, order: DESC}) {
+      nodes {
+        category
+        date(formatString: "do MMM, YYYY")
+        description
+        id
+        slug
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog

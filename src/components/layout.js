ba870166b163a3523db384e229e572f4e47d666d
shauryakalia <shauryakalia@gmail.com>
2021-04-11 20:30:27 +0530

    sidebar

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,9 +5,14 @@ import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  }
   return (
     <>
-      <Navbar />
+      <Navbar toggleSidebar={toggleSidebar}/>
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
       {children}
       <Footer />
     </>

5781a77d1aa6e1feb295308334278d58e0338cf8
shauryakalia <shauryakalia@gmail.com>
2021-04-18 21:56:12 +0530

    error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,10 @@ import SEO from "../components/SEO"
 
 const Error = () => {
   return <main className="error-page">
-
+    <div className="error-container">
+      <h1>oop's it's a dead end</h1>
+      <Link to="/" className="btn">Take me back to Home</Link>
+    </div>
   </main>
 }
 

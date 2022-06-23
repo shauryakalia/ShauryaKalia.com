dbd743e4143a81efd4a5f99a7ac51dc73a59004c
shauryakalia <shauryakalia@gmail.com>
2022-06-24 03:58:12 +0530

    disquss plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,13 +29,19 @@ module.exports = {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: `http://localhost:1337`,
-        queryLimit: 1000, // Default to 100
+        queryLimit: 100, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
         collectionTypes: [`experiences`, `projects`, `blogs`],
         singleTypes: [`about`],
       },
+    },
+    {
+      resolve: `gatsby-plugin-disqus`,
+      options: {
+          shortname: `shauryakalia`
+      }
     },
     // {
     //   resolve: `gatsby-plugin-webfonts`,
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -2,20 +2,29 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import ReactMarkdown from "react-markdown"
+import { Disqus } from 'gatsby-plugin-disqus';
 
-const ComponentName = ({data}) => {
-  const {content} = data.blog
+const ComponentName = ({ data }) => {
+  const { content } = data.blog
+  const disqusConfig = {
+    url: window.location.href,
+    identifier: window.location.pathName,
+    title: window.location.pathName,
+  }
   return <Layout>
-     <section className="blog-template">
-       <div className="section-center">
-         <article className="blog-content">
-           <ReactMarkdown source={content} />
-         </article>
-         <Link to="/blog" className="btn enter-btn">
-           blogs
-         </Link>
-       </div>
-     </section>
+    <section className="blog-template">
+      <div className="section-center">
+        <article className="blog-content">
+          <ReactMarkdown source={content} />
+          <Disqus
+            config={disqusConfig}
+        />
+        </article>
+        <Link to="/blog" className="btn center-btn">
+          blogs
+        </Link>
+      </div>
+    </section>
   </Layout>
 }
 
